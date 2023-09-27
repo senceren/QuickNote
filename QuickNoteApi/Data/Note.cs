79e5d84855ad2d365c3ee93cee83c8ceeaa3031f
senceren <cerensen222@gmail.com>
2023-09-27 12:49:34 +0300

    Add project files.

---
+++ QuickNoteApi/Data/Note.cs
@@ -0,0 +1,9 @@
+﻿namespace QuickNoteApi.Data
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string Content { get; set; }
+    }
+}
---
+++ QuickNoteApi/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.EntityFrameworkCore;
+using QuickNoteApi.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(ob => ob.UseSqlServer(builder.Configuration.GetConnectionString("ApplicationDbContext")
+    ));
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(s => s.AddDefaultPolicy(p => p.AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin()));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate(); // update-database demeye gerek kalmadan sunucuda doðrudan çalýþtýrmaya yarýyor.Ýlk migrationý yapýyoruz ve sonraýndaki deðiþikliklerde gerek kalmýyor.
+}
+
+    app.Run();
